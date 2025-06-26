3c9adacc27007986774353adea17676b44e91006
arjav0703 <arjavjain0703@gmail.com>
2025-06-26 18:11:32 +0530

    get tauri to work

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,21 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+// // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn my_custom_command(invoke_message: String) {
+    println!(
+        "I was invoked from JavaScript with message: {}",
+        invoke_message
+    );
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![my_custom_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
