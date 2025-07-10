f2c26ff845c6c20c963e90e1160b4a427e111169
arjav0703 <arjavjain0703@gmail.com>
2025-07-10 16:33:53 +0530

    lint

--- components/AudioControls.ts
+++ components/AudioControls.ts
@@ -1,29 +1,29 @@
 import { Dispatch, RefObject, SetStateAction } from "react";
 
 
-  const play = (audioRef: RefObject<HTMLAudioElement | null> , setIsPlaying: Dispatch<SetStateAction<boolean>>
+const play = (audioRef: RefObject<HTMLAudioElement | null>, setIsPlaying: Dispatch<SetStateAction<boolean>>
 ) => {
-    audioRef.current?.play();
-    setIsPlaying(true);
-  };
+  audioRef.current?.play();
+  setIsPlaying(true);
+};
 
-  // const pause = () => {
-  //   audioRef.current?.pause();
-  //   setIsPlaying(false);
-  // };
+// const pause = () => {
+//   audioRef.current?.pause();
+//   setIsPlaying(false);
+// };
 
-  // const next = () => {
-  //   const nextIdx = (current + 1) % playlist.length;
-  //   setCurrent(nextIdx);
-  //   setIsPlaying(true);
-  //   persist(playlist, nextIdx);
-  // };
+// const next = () => {
+//   const nextIdx = (current + 1) % playlist.length;
+//   setCurrent(nextIdx);
+//   setIsPlaying(true);
+//   persist(playlist, nextIdx);
+// };
 
-  // const prev = () => {
-  //   const prevIdx = (current - 1 + playlist.length) % playlist.length;
-  //   setCurrent(prevIdx);
-  //   setIsPlaying(true);
-  //   persist(playlist, prevIdx);
-  // };
+// const prev = () => {
+//   const prevIdx = (current - 1 + playlist.length) % playlist.length;
+//   setCurrent(prevIdx);
+//   setIsPlaying(true);
+//   persist(playlist, prevIdx);
+// };
 
 export default play
