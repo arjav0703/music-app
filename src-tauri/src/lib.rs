48bf52415f6efa57165c51c5aeb290278fc9931a
arjav0703 <arjavjain0703@gmail.com>
2025-07-13 17:05:01 +0530

    pkg: add notification plugin for tauri

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,7 @@ use tauri_plugin_store::StoreExt;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
         .plugin(
             tauri_plugin_log::Builder::new()
                 .target(tauri_plugin_log::Target::new(
