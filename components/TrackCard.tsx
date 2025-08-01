135925f92e8dcb076a75204ffb3d54b57f84721f
Arjav Jain <arjavjain0703@gmail.com>
2025-07-31 21:17:53 -0400

    fuck css

--- components/TrackCard.tsx
+++ components/TrackCard.tsx
@@ -11,21 +11,21 @@ export default function TrackCard({ cover, title, artist, onClick }: Props) {
   return (
     <Card
       onClick={onClick}
-      className="cursor-pointer group transition-all duration-200 hover:shadow-lg hover:scale-[1.03] bg-neutral-900 border border-neutral-800 rounded-2xl overflow-hidden"
+      className="cursor-pointer group transition-all duration-200 hover:scale-[1.03] bg-neutral-900 border border-neutral-800 rounded-2xl overflow-hidden  w-[280px] h-auto"
     >
-      <div className="relative aspect-square w-full overflow-hidden">
+      <div className="relative aspect-square w-full h-full overflow-hidden">
         {cover ? (
           <img
             src={cover}
             alt={title}
-            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+            className="w-full "
           />
         ) : (
           <div className="w-full h-full flex items-center justify-center bg-neutral-700 text-neutral-300 text-sm">
             No Cover
           </div>
         )}
-        <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent" />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-transparent" />
       </div>
 
       <CardContent className="p-3 text-white">
--- components/TrackGrid.tsx
+++ components/TrackGrid.tsx
@@ -15,12 +15,12 @@ type Props = {
 
 export default function TrackGrid({ tracks, onSelect }: Props) {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-6">
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
       {tracks.map((track, index) => (
         <InView
           key={index}
           triggerOnce
-          rootMargin="300px"
+          rootMargin="500px"
         >
           {({ inView, ref }) => (
             <div ref={ref}>
